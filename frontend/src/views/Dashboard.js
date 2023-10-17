088449bb545047cbb7b5e69fb596c8807cefdd32
heejung1180 <heejung1180@outlook.com>
2023-10-16 17:09:33 -0700

    added icons to dashboard/homepage and create file for friend group and add friend

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -2,6 +2,8 @@ import styles from '../styles/Dashboard.module.css';
 import { PassageAuthGuard } from '@passageidentity/passage-react';
 import { usePassageUserInfo } from '../hooks/';
 import LogoutButton from '../components/LogoutButton';
+import { BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom';
+import FriendGroup from './FriendGroup';
 
 function Dashboard() {
   const { userInfo, loading } = usePassageUserInfo();
@@ -26,13 +28,29 @@ function Dashboard() {
       }
     >
       <div className={styles.dashboard}>
+      <span class="material-symbols-outlined">
+        account_circle
+      </span>
         <div className={styles.title}>Welcome</div>
+        <p>to</p>
+        <div className={styles.title}>Phone a Friend!</div>
+        <div>
+          <span class="material-symbols-outlined" >
+            group
+          </span>
+          <span class="material-symbols-outlined">
+            add_call
+          </span>
+          <span class="material-symbols-outlined">
+            calendar_add_on
+          </span>
+        </div>
+        <br />
         <div className={styles.message}>
-          You successfully signed in with Passage. This is your homepage. <br />
-          <br />
-          Your username is: {userInfo?.email}
+          You successfully signed in with Passage. Your username is: {userInfo?.email}
         </div>
         <LogoutButton />
+        
       </div>
     </PassageAuthGuard>
   );
