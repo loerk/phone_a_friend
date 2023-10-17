4f733423c5eae27c94629a0b6c7b64ec70ae50e0
heejung1180 <heejung1180@outlook.com>
2023-10-17 14:26:22 -0700

    created routes and links to friendgroup and calendar

--- frontend/src/views/FriendGroup.js
+++ frontend/src/views/FriendGroup.js
@@ -1,23 +1,17 @@
 import styles from '../styles/Dashboard.module.css';
 
 
-function FriendGroup() {
+export default function FriendGroup() {
 
   return (
 
-      <div className={styles.dashboard}>
-        <span class="material-symbols-outlined">
-        person_add
-        </span>
-        <div className={styles.title}>Your Friend Group</div>
-        <div>
+      <div>
+        
+        <div>Your Friend Group</div>
 
-        </div>
        
         
       </div>
     
   )
 }
-
-export default FriendGroup;
