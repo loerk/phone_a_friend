5ac73f1d2f7d1ce0c44d2a8015603877f4743826
heejung1180 <heejung1180@outlook.com>
2023-10-16 17:09:33 -0700

    added icons to dashboard/homepage and create file for friend group and add friend

---
+++ frontend/src/views/FriendGroup.js
@@ -0,0 +1,23 @@
+import styles from '../styles/Dashboard.module.css';
+
+
+function FriendGroup() {
+
+  return (
+
+      <div className={styles.dashboard}>
+        <span class="material-symbols-outlined">
+        person_add
+        </span>
+        <div className={styles.title}>Your Friend Group</div>
+        <div>
+
+        </div>
+       
+        
+      </div>
+    
+  )
+}
+
+export default FriendGroup;
