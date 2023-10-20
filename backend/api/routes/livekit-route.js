64c21213002f4e6c9425255d32288ce0016f7379
Claire Abu-Assal <claire@hex.tech>
2023-10-20 13:12:59 -0700

    Make room token user specific.

--- backend/api/routes/livekit-route.js
+++ backend/api/routes/livekit-route.js
@@ -2,23 +2,24 @@ const express = require('express');
 const router = express.Router();
 const livekitServer = require('livekit-server-sdk');
 
-const createToken = () => {
+const createToken = (userId) => {
   // if this room doesn't exist, it'll be automatically created when the first
   // client joins
   const roomName = 'quickstart-room';
   // identifier to be used for participant.
   // it's available as LocalParticipant.identity with livekit-client SDK
-  const participantName = 'quickstart-username';
-
-  const at = new livekitServer.AccessToken('devkey', 'secret', {
-    identity: participantName
+  const accessToken = new livekitServer.AccessToken('devkey', 'secret', {
+    identity: userId,
   });
-  at.addGrant({ roomJoin: true, room: roomName });
-  return at.toJwt();
-};
 
-router.route('/roomtoken').get((req, res) => {
-  res.send(createToken());
-});
+  accessToken.addGrant({ roomJoin: true, room: roomName });
+  return accessToken.toJwt();
+}
+
+router
+  .route('/roomtoken/:userId')
+  .get((req, res) => {
+    res.send(createToken(req.params.userId));
+  });
 
 module.exports = router;
