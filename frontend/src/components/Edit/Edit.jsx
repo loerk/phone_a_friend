9db48c94219e7b363b14f871ac6ca84d7c262df6
heejung1180 <heejung1180@outlook.com>
2023-10-22 11:06:52 -0700

    added module.css to components for better css control

--- frontend/src/components/Edit/Edit.jsx
+++ frontend/src/components/Edit/Edit.jsx
@@ -1,4 +1,4 @@
-import '../HomePage/HomePage.css'
+import styles from './Edit.module.css'
 import { Link } from 'react-router-dom'
 import FriendGroup from '../FriendGroup/FriendGroup'
 
@@ -6,36 +6,35 @@ export default function Edit() {
 
   return (
     <>
-      <div>
-        <div>
-          <h1 className='maintitle'>Edit</h1>
+      <div className={styles.center}>
+        <div >
+          <h1 className={styles.mainTitle}>Edit</h1>
         </div>
-        <div className='frEditContainer'>
-          
-          <div className='editBtnContainer'>
+        <div className={styles.container}>          
+          <div className={styles.right}>
             <Link to="/friendgroup">
-            <span className="friendIcon material-symbols-outlined">
-              save
-            </span>
+              <span className={styles.smallIcon}>
+                save
+              </span>
             </Link>
             <Link to="/friendgroup">
-            <span className="friendIcon material-symbols-outlined">
-              cancel
-            </span>
+              <span className={styles.smallIcon}>
+                cancel
+              </span>
             </Link>
           </div>
           <div>
-            <div className='editNameContainer'>
-              <div className='title'>Name: </div>
-              <div className='entry'>Emily Cooper</div>
+            <div className={styles.nameContainer}>
+              <div className={styles.title}>Name: </div>
+              <div className={styles.entry}>Emily Cooper</div>
             </div>
-            <div className='editNameContainer'>
-              <div className='title'>Email: </div>
-              <div className='entry'>emily@email.com</div>
+            <div className={styles.nameContainer}>
+              <div className={styles.title}>Email: </div>
+              <div className={styles.entry}>emily@email.com</div>
             </div>
-            <div className='editNameContainer'>
-              <div className='title'>Phone: </div>
-              <div className='entry'>(213) 456-7890</div>
+            <div className={styles.nameContainer}>
+              <div className={styles.title}>Phone: </div>
+              <div className={styles.entry}>(213) 456-7890</div>
             </div>
           </div>
         </div>       
