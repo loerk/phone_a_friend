bd3306b000889083af2ac36575de7e38f06406b2
Lianna <liannanovitz@gmail.com>
2023-10-21 17:55:50 -0700

    mock 2 users WIP

--- frontend/src/components/PhoneCall/PhoneCall.jsx
+++ frontend/src/components/PhoneCall/PhoneCall.jsx
@@ -3,7 +3,6 @@ const { LiveKitRoom } = require('@livekit/components-react');
 //import { fetchData } from '../../api/fetcher';
 
 // import
-
 //const makeCall = async () => {
 //   console.log('makeCall');
 //   const fakeId = '123';
@@ -24,15 +23,23 @@ const { LiveKitRoom } = require('@livekit/components-react');
 //     console.log('Error ', error);
 //   }
 // };
+console.log(this);
 
-export default function PhoneCall() {
+export default function PhoneCall({ user }) {
   //console.log('makeCall fake call ', makeCall);
+  //const user = '123';
+  //const friend = '456';
 
-  const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6ImhhcHB5LWZyb2ctZGFuY2UyIn0sImlhdCI6MTY5NzkzMTA4OSwibmJmIjoxNjk3OTMxMDg5LCJleHAiOjE2OTc5NTI2ODksImlzcyI6ImRldmtleSIsInN1YiI6IjEyMyIsImp0aSI6IjEyMyJ9.hgfJqSxLm5BHySLf46kXN2_gnxeH_1if2xrsfZ_CVqo';
+  const fakeToken1 =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTgwMjIwNzIsImlzcyI6ImRldmtleSIsIm5hbWUiOiJ1c2VyMSIsIm5iZiI6MTY5NzkzNTY3Miwic3ViIjoidXNlcjEiLCJ2aWRlbyI6eyJyb29tIjoiaGFwcHktZnJvZy1kYW5jZTIiLCJyb29tSm9pbiI6dHJ1ZX19.HnzZ6vBB6GKgpL3u2kC06Y7VZBwJdMUis8inOGFGZyU';
+  const fakeToken2 =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTgwMjIwOTYsImlzcyI6ImRldmtleSIsIm5hbWUiOiJ1c2VyMiIsIm5iZiI6MTY5NzkzNTY5Niwic3ViIjoidXNlcjIiLCJ2aWRlbyI6eyJyb29tIjoiaGFwcHktZnJvZy1kYW5jZTIiLCJyb29tSm9pbiI6dHJ1ZX19.OFZPgiiGsaLn1eKKkd41y4kEU-5X2kpFDVOKWZq6xvs';
+  let token;
+  if (user == '123') token = fakeToken1;
+  else token = fakeToken2;
+
   const wsUrl = 'ws://localhost:7880/';
 
-  console.log('token', token);
   console.log('wsUrl ', wsUrl);
   return (
     // livekit component
